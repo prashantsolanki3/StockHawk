2bfcf042590cc12f904ee78800f11686602b4052
Prashant Solanki <solankisrp2@gmail.com>
2016-05-24 12:06:52 +0530

    Upgraded Gradle, Support Lib and GMS versions, Added Crashlytics and Answers, Created Application Singleton

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/App.java
@@ -0,0 +1,25 @@
+package com.sam_chordas.android.stockhawk;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Package com.sam_chordas.android.stockhawk
+ * <p/>
+ * Created by Prashant on 5/24/2016.
+ * <p/>
+ * Email: solankisrp2@gmail.com
+ * Github: @prashantsolanki3
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics(),new Answers());
+    }
+}
